50ce7ab1cdd4dd8a2c32988efa70a3318f7fc28e
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-05-20 12:05:13 +0530

    control statements completed

--- 14_control_statements.py
+++ 14_control_statements.py
@@ -139,3 +139,113 @@ index = 0
 
 #--------------------------
 
+#------ for loop:-
+
+# Write a program to print "Hello World" 5 times.
+
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+
+# count = 0
+
+# while count < 5:
+#     print("Hello World")
+#     count+=1
+
+# for count in range(0, 5):
+#     print("Hello World")
+
+
+#----------------------
+# Write a program to print tabale of 9
+
+# for count in range(1, 11):
+#     print(f"9 * {count} = {9*count}")
+
+
+#--------------------
+# Write a program to print all odd numbers 
+# from 50 to 70 
+
+# for num in range(50, 71):
+#     if num % 2 != 0:
+#         print(num)
+#     else:
+#         pass
+
+#----------------------------
+# Write a program to print all numbers from a str1
+# str1 = "Ah4&*io)012'"
+# # print(len(str1))
+# for index in range(0, len(str1)):
+#     if str1[index].isdigit():
+#         print(str1[index])
+        
+
+# Write a program to print all numbers divisible by 7 
+# from list1
+
+list1 = [45, 12, 14, 56, 81, 90, 77, 84]
+
+# for num in list1:
+#     if num % 7 == 0:
+#         print(num)
+
+
+# for index in range(0, len(list1)):
+#     if list1[index] % 7 == 0:
+#         print(list1[index])
+
+
+#--------------------
+# Write a program to print user specified number of 
+# fibonacci series.
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233
+
+
+# num_of_nums = int(input("How many numbers of fibonacci series you want: "))
+# num1 = 0
+# num2 = 1
+# print(num1)
+# print(num2)
+
+# for i in range(2, num_of_nums):
+#     new = num1 + num2
+#     print(new)
+#     num1, num2 = num2, new
+
+
+# -----------------------------
+
+# print even and divisble by 4 numbers in between 90 to 70.
+
+
+# for num in range(90, 70, -1):
+#     if num % 4 == 0:
+#         print(num)
+
+
+#------------------
+
+# Write a program to print all armstrong numbers in between 
+# 100 and 10000
+
+# 153 ==> (1** 3) + (5**3) + (3**3)
+#     ==> 1 + 125 + 27
+#     ==> 153
+
+#-------------------------
+
+# for num in range(100, 10000):
+#     power = len(str(num))
+#     total = 0
+#     for digit in str(num):
+#         total += (int(digit) ** power)
+#     if total == num:
+#         print(num)
+
+#------------------------------
