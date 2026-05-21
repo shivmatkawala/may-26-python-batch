313640ae2d3fd140bb0a5ec857645eeff049cdf1
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-05-21 11:33:20 +0530

    comprehensions completed

--- 15_comprehensions.py
+++ 15_comprehensions.py
@@ -1,87 +1,84 @@
-# Comprhensions:-
-
-    # What is comprehesions:-
-        # it is a form of collection where program is written.
-        # that program generates data / elements
+# Comprehensions:-
 
 
-    # Comprehesions are easy write and read
-    # Comprehensions are aslo easy to maintain
-    # they are faster.
+# list1 = [2, 4, 6, 8, 10]
 
-    # Comprensions are suitable for simple logic.
-
-
-# write a program to generate table of 19 and store it in list.
-# table_of_19 = []
-
+# evans = []
 # for i in range(1, 11):
-#     table_of_19.append(i* 19)
+#     if i % 2 == 0:
+#         evans.append(i)
+# print(evans)
 
-# print(table_of_19) 
+# List Comprehension:
 
-# List Comprehensions:-
-
-# table_of_29 = [i * 29 for i in range(1, 11)]
-# print(table_of_29)
-
-
-# write a list coprehension which contains all even numbers
-# E in between 1 to 20
-
-# evens = [i for i in range(0, 21, 2)]
+# evens = [num for num in range(1, 11) if num % 2 == 0]
 # print(evens)
 
-# evens = [i for i in range(1, 21) if i%2 == 0 ]
-# print(evens)
+#--------------------------------
 
-# str1 = "Gautemala Europe123%^&*"
+# write a list comprehension to get all numbers in between 20 to 50
+# divisible by 3
 
-# use str1 and create a list comprehension of consonens
+# divisible_by_3 = [num for num in range(20, 50) if num % 3 == 0]
+# print(divisible_by_3)
 
-# consonents = [i for i in str1 if i not in "aeiouAEIOU" and i.isalpha()]
+# Write a list comprehension to get the consonents from
+# str1 string.
+
+# str1 = "782FDGimaO"
+
+# consonents = [char for char in str1 if char.isalpha() and char not in "AEIOUaeiou"]
 # print(consonents)
 
+#--------------------------------
 
-# Write a list comprehension to get all the odd numbers 
-# which are divisible by 3 and 7 in between of 1 and 100
+# Write a list comprehension to get the all 
+# odd numbers in between 100 and 10 which are
+# divisible by 3 and 7
 
-# xyz = [num for num in range(1, 100) if num % 2 != 0 and num % 3 == 0 and num % 7 == 0]
-# print(xyz)
-
-# Write a program to get all numbers which are divisble by 
-# 5 from str1
-
-# str1 = "Film@#$750"
-
-# result = [num for num in str1 if num.isnumeric() and int(num) % 5 == 0]
-# print(result)
-
-#-------------------
-
-# tuple comprehension:-
-
-# Write a tuple comprehension to print 1 to 10 numbers.
-
-# tup1 = (i for i in range(1, 11))
-# print(tup1)
-# for i in tup1:
-#     print(i, end=" ")
-
-# Set Comprehension:
-
-#------------------
-# s1 = {num for num in range(20, 10, -2)}
-# print(s1)
+# odds = [num for num in range(100, 10, -1) if num % 2 != 0 and num % 3 == 0 and num % 7 == 0]
+# print(odds)
 
 
-# dictionary Comprehension:
+# write a list comprehension which contains day, month, and year
+#ask user to provide dob(mm/dd/yyyy) you print 
+# dob = input("enter dob: ")
 
-# Write a dict comprehension to get number 
-# as a key and its square as a value  from 1 to 10
+# list1 = [int(value) for value in dob.split("/")]
+# print(list1)
 
-# dict1 = {1:1, 2:4, 3:9, 4:16}
 
-# dict1 = {key: key**2 for key in range(1, 11)}
+# -------------------------
+# Tuple Comprehension:-
+
+# use tuple comprehension and create a table of 19
+
+# table_of_19 = (num*19 for num in range(1, 11))
+
+# print(table_of_19)  #<generator object <genexpr> at 0x0000020DAE909490>
+
+# for i in table_of_19:
+#     print(i)
+
+
+#------------------------------
+# Set comprehension: 
+
+# set1 = {num for num in range(1, 100) if num % 5 == 0}
+# print(set1)
+
+
+# Dictionary comprehension:
+
+# write a dict comoprehehsnion to get number as 
+# a key and its square as a value in between 1 and 10
+
+# dict1 = {key:key**2 for key in range(1, 11)}
 # print(dict1)
-#-------------------------------------------
+
+# create a dictionary comprehnsion of all charecters from str1
+# as a key and its ascii code as Value
+# str1 = "hello@123"
+# dict2 = {key:ord(key) for key in str1}
+# print(dict2)
+
