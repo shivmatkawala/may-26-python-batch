cfdb00a2bdb7a44d20dac69f87cdd327055473e0
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-06-03 17:59:35 +0530

    started numpy

---
+++ 20_numpy_module.py
@@ -0,0 +1,53 @@
+# Numpy:-
+    # Numeric  Python
+    # Open Source
+    # Numpy is the fundamental library for scientific computing
+    # in python. It provides
+        # Fast Multidiamensional Array.
+        # Mathematical Operations
+        # Linear Algebra
+        # Statistical Functions
+        # Random Number Generation
+        # Broadcasting
+        # Itegration with data science libraries, dat engineering Libraries
+            # Pandas, SciPy, Sckit-learn
+
+# Import numpy module in file where you want to use it.
+
+import numpy as np
+
+
+# Using numpy create an array:
+
+# ar1 = np.array((1, 2, 3, 4, 5))
+# print(ar1)
+
+# ar2 = np.array([1, 2, 3, 4, 5])
+# print(ar2)
+
+#---------------------
+# some attributes
+
+# shape
+
+# l1 = [1, 2, 3, 4, 5, 6]   # linear , 1 diamensional
+
+# l2 = [[1, 2], [3, 4], [5, 6]] # non-linear. 2 ddiamensiaonal
+
+# l3 = [
+#     [
+#         [1, 2, 3],
+#         [4, 5, 6]
+#     ],
+#     [
+#         [7, 8, 9],
+#         [10, 11, 12]
+#     ]
+# ]
+
+#------------------------
+
+names = np.array(["Alan", "sara", "Jaya"])
+ages = np.array([34, 32, 25])
+salary = np.array([50000, 60000, 100000])
+positions = np.array(["JD", "JD", "SD"])
