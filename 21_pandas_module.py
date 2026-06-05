8503464e187faf08bcf2c72f28696f9b4faf9b3b
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-06-05 18:43:27 +0530

    pandas started

---
+++ 21_pandas_module.py
@@ -0,0 +1,19 @@
+# Pandas:-
+
+    # Pandas is the most popular library of python used
+    # for Data Analysis and Manipulation.
+
+    # It provides tow main data structures:
+        # Series: 1- diamension labelled array
+        # Dataframe:- 2-diamensional table (similar to SQL or excel table)
+
+    # Pandas is built on top of Numpy and is heavily used in:
+        # Data Analysis
+        # Data Cleaning
+        # ETL Pipelines
+        # Machine Learning
+        # Financial Analysis
+        # Reporting
+
+
+# Installation of Pandas:
